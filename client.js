361698ddf5aed6cb6e151d1dbafce8a5d0eeff64
Heather Kim <heatherkim0405@gmail.com>
2022-01-25 16:29:43 -0600

    completed the main goal

--- client.js
+++ client.js
@@ -39,4 +39,84 @@ const employees = [
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
 
-console.log( employees );
+// console.log(employees);
+let output = [];
+
+for (employee of employees) {
+  let employeeOutput = employeeInput(employee);
+  console.log(employeeOutput);
+}
+
+
+
+function employeeInput(employee) {
+  let percentage = 0;
+  let rating = employee.reviewRating;
+  let salary = Number(employee.annualSalary);
+  let name = employee.name;
+  let employeeNumber = employee.employeeNumber;
+  let totalBonus = 0;
+  let totalCompensation = 0;
+  percentage = calBonusPercentage(rating, salary, employeeNumber)
+  totalBonus = calBonusAmount(percentage, salary);
+  totalCompensation = Math.round(salary + totalBonus);
+  let person = new Employee(name, percentage, totalCompensation, totalBonus);
+  output.push(person)
+  return person;
+}
+
+// employeeInput(employee);
+
+
+
+function calBonusPercentage(rating, salary, employeeNumber) {
+  let percentage = 0;
+  if (rating <= 2) {
+    percentage = 0;
+  } else if (rating === 3) {
+    percentage = 0.04;
+  } else if (rating === 4) {
+    percentage = 0.06;
+  } else if (rating === 5) {
+    percentage = 0.10;
+  }
+  if (employeeNumber.length <= 4) {
+    percentage += 0.05;
+  }
+  if (salary > 65000) {
+    percentage -= 0.01;
+  }
+  if (percentage > .13) {
+    percentage = .13
+  }
+  if (percentage < 0) {
+    percentage = 0
+  }
+  percentage = percentage.toFixed(2);
+  percentage = Number(percentage);
+  return percentage;
+
+}
+
+function calBonusAmount(percentage, annualSalary) {
+  let totalBonus = 0;
+  let totalCompensation = Number(employee.annualSalary);
+  totalBonus = percentage * annualSalary;
+  totalBonus = Math.round(totalBonus);
+  return totalBonus;
+}
+
+
+
+function Employee(name, bonusPercentage, totalCompensation, totalBonus) {
+  this.name = name;
+  this.bonusPercentage = bonusPercentage;
+  this.totalCompensation = totalCompensation;
+  this.totalBonus = totalBonus;
+}
+
+
+
+console.log(output);
+
+// console.log(employees[1].employeeNumber.length);
